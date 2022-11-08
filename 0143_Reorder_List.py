03d6abaf51a8d2eb35076cfadab9cef06af46c46
thomwebb <webb.thomasj@gmail.com>
2022-11-08 12:55:05 -0800

    add problem description

--- 0143_Reorder_List.py
+++ 0143_Reorder_List.py
@@ -1,6 +1,15 @@
 '''
 Link to problem: https://leetcode.com/problems/reorder-list/
 
+### Problem Description
+You are given the head of a singly linked-list. The list can be represented as:
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+### End Description
+
+
 # Approach
 - We will use a double-ended queue (`deque`) to maintain the original order of the nodes
 - We will alternate popping from the right and left sides of the queue to get the desired reorder
