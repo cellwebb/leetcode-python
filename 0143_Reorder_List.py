fef74413ec52022030c59c755c068ffd0747cbe4
thomwebb <webb.thomasj@gmail.com>
2022-11-08 12:46:44 -0800

    Create 0143_Reorder_List.py

---
+++ 0143_Reorder_List.py
@@ -0,0 +1,48 @@
+'''
+Link to problem: https://leetcode.com/problems/reorder-list/
+
+# Approach
+- We will use a double-ended queue (`deque`) to maintain the original order of the nodes
+- We will alternate popping from the right and left sides of the queue to get the desired reorder
+- Once we deplete the queue, we will set the last nodes `.next` attribute to `None` so there isn't a cycle in the list
+
+My solution on leetcode: https://leetcode.com/problems/reorder-list/solutions/2794563/python3-simple-explanation-faster-than-95-55-double-ended-queue-90-ms/
+
+Execution time: 90 ms (faster than 95.55%)
+Memory usage: 24.2 MB (smaller than 27.62%; best is 22 MB)
+Time complexity: O(n)
+Space complexity: O(n)
+
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+from collections import deque
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """Do not return anything, modify head in-place instead."""
+        
+        node = head
+        q = deque()  # we will not add the head to the queue
+        while node.next:
+            node = node.next
+            q.append(node)
+        
+        node = head
+        while q:
+            node.next = q.pop()
+            node = node.next
+            if q:
+                node.next = q.popleft()
+                node = node.next
+        
+        node.next = None
+
+```
+
+Please upvote if you found this helpful! :)
