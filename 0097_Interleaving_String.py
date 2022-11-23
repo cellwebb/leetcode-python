25227a18951f83e483f3b1d74eddfcd32a8f3799
thomwebb <webb.thomasj@gmail.com>
2022-11-22 17:32:32 -0800

    Update 0097_Interleaving_String.py

--- 0097_Interleaving_String.py
+++ 0097_Interleaving_String.py
@@ -8,6 +8,8 @@ Memory: 16.9 MB
     
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3): return False
+        
         cache = {("", "", ""): True}
 
         def helper(s1: str, s2: str, s3: str) -> bool:
@@ -15,7 +17,6 @@ class Solution:
 
             if not s1: return s2 == s3
             if not s2: return s1 == s3
-            if not s3 and (s1 or s2): return False
 
             cache[(s1, s2, s3)] = (
                 s1[0] == s3[0] and helper(s1[1:], s2, s3[1:]) or
